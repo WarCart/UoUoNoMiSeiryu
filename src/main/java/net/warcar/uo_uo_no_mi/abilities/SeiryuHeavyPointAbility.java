4fde54fe7e4db1fb33b1dcc0711827902a4f7a7f
Warcar <@imasanov2007@gmail,com>
2024-09-26 19:45:50 +0300

    Heavy Point Modifiers Fixed

--- src/main/java/net/warcar/uo_uo_no_mi/abilities/SeiryuHeavyPointAbility.java
+++ src/main/java/net/warcar/uo_uo_no_mi/abilities/SeiryuHeavyPointAbility.java
@@ -5,12 +5,10 @@ import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.potion.EffectInstance;
 import net.warcar.uo_uo_no_mi.init.UoMorphs;
-import xyz.pixelatedw.mineminenomi.api.abilities.AbilityAttributeModifier;
-import xyz.pixelatedw.mineminenomi.api.abilities.AbilityCategory;
-import xyz.pixelatedw.mineminenomi.api.abilities.AbilityCore;
-import xyz.pixelatedw.mineminenomi.api.abilities.MorphAbility2;
+import xyz.pixelatedw.mineminenomi.api.abilities.*;
 import xyz.pixelatedw.mineminenomi.api.helpers.AttributeHelper;
 import xyz.pixelatedw.mineminenomi.api.morph.MorphInfo;
+import xyz.pixelatedw.mineminenomi.data.entity.ability.AbilityDataCapability;
 import xyz.pixelatedw.mineminenomi.init.ModAttributes;
 import xyz.pixelatedw.mineminenomi.init.ModEffects;
 
@@ -21,20 +19,32 @@ public class SeiryuHeavyPointAbility extends MorphAbility2 {
 
     public SeiryuHeavyPointAbility(AbilityCore<SeiryuHeavyPointAbility> core) {
         super(core);
+        this.continuousComponent.addStartEvent(this::onStart);
         this.statsComponent.addAttributeModifier(ModAttributes.TOUGHNESS, new AbilityAttributeModifier(AttributeHelper.MORPH_TOUGHNESS_UUID, INSTANCE, "Seiryu Heavy Point Toughness Modifier", 120, AttributeModifier.Operation.ADDITION), entity -> this.isContinuous());
         this.statsComponent.addAttributeModifier(Attributes.ARMOR, new AbilityAttributeModifier(AttributeHelper.MORPH_ARMOR_UUID, INSTANCE, "Seiryu Heavy Point Armor Modifier", 30, AttributeModifier.Operation.ADDITION), entity -> this.isContinuous());
-        this.statsComponent.addAttributeModifier(Attributes.MAX_HEALTH, new AbilityAttributeModifier(AttributeHelper.MORPH_HEALTH_UUID, INSTANCE, "Seiryu Heavy Point Health Modifier", 50, AttributeModifier.Operation.ADDITION), entity -> this.isContinuous() && !isFullyDrunk(entity));
-        this.statsComponent.addAttributeModifier(Attributes.ATTACK_DAMAGE, new AbilityAttributeModifier(AttributeHelper.MORPH_STRENGTH_UUID, INSTANCE, "Seiryu Heavy Point Damage Modifier", 10, AttributeModifier.Operation.MULTIPLY_TOTAL), entity -> this.isContinuous() && !isFullyDrunk(entity));
-        this.statsComponent.addAttributeModifier(Attributes.MAX_HEALTH, new AbilityAttributeModifier(UUID.fromString("c35bc0b9-16e5-4010-a3e0-eb41cd18b5a7"), INSTANCE, "Seiryu Heavy Point Health Modifier", 75, AttributeModifier.Operation.ADDITION), entity -> this.isContinuous() && isFullyDrunk(entity));
-        this.statsComponent.addAttributeModifier(Attributes.ATTACK_DAMAGE, new AbilityAttributeModifier(UUID.fromString("90d99ba5-e3f6-4873-8b19-697ff2ecdbf9"), INSTANCE, "Seiryu Heavy Point Damage Modifier", 15, AttributeModifier.Operation.MULTIPLY_TOTAL), entity -> this.isContinuous() && isFullyDrunk(entity));
+    }
+
+    private void onStart(LivingEntity livingEntity, IAbility ability) {
+        if (isFullyDrunk(livingEntity)) {
+            this.statsComponent.addAttributeModifier(Attributes.MAX_HEALTH, new AbilityAttributeModifier(UUID.fromString("c35bc0b9-16e5-4010-a3e0-eb41cd18b5a7"), INSTANCE, "Seiryu Heavy Point Health Modifier", 75, AttributeModifier.Operation.ADDITION), entity -> this.isContinuous());
+            this.statsComponent.addAttributeModifier(Attributes.ATTACK_DAMAGE, new AbilityAttributeModifier(UUID.fromString("90d99ba5-e3f6-4873-8b19-697ff2ecdbf9"), INSTANCE, "Seiryu Heavy Point Damage Modifier", 15, AttributeModifier.Operation.MULTIPLY_TOTAL), entity -> this.isContinuous());
+        } else {
+            this.statsComponent.addAttributeModifier(Attributes.MAX_HEALTH, new AbilityAttributeModifier(AttributeHelper.MORPH_HEALTH_UUID, INSTANCE, "Seiryu Heavy Point Health Modifier", 50, AttributeModifier.Operation.ADDITION), entity -> this.isContinuous());
+            this.statsComponent.addAttributeModifier(Attributes.ATTACK_DAMAGE, new AbilityAttributeModifier(AttributeHelper.MORPH_STRENGTH_UUID, INSTANCE, "Seiryu Heavy Point Damage Modifier", 10, AttributeModifier.Operation.MULTIPLY_TOTAL), entity -> this.isContinuous());
+        }
     }
 
     public MorphInfo getTransformation() {
         return UoMorphs.SEIRYU_HEAVY.get();
     }
 
-    private static boolean isFullyDrunk(LivingEntity entity) {
+    public static boolean isFullyDrunk(LivingEntity entity) {
         EffectInstance instance = entity.getEffect(ModEffects.DRUNK.get());
-        return instance != null && instance.getAmplifier() >= 4;
+        return isActive(entity) &&  instance != null && instance.getAmplifier() >= 4;
+    }
+
+    public static boolean isActive(LivingEntity entity) {
+        SeiryuHeavyPointAbility ability = AbilityDataCapability.get(entity).getEquippedAbility(INSTANCE);
+        return ability != null && ability.isContinuous();
     }
 }
