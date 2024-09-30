2b1545b98343857a13858a13e501a97299da6adb
Warcar <@imasanov2007@gmail,com>
2024-09-30 18:55:03 +0300

    Fixed Kaifu

--- src/main/java/net/warcar/uo_uo_no_mi/abilities/KaifuAbility.java
+++ src/main/java/net/warcar/uo_uo_no_mi/abilities/KaifuAbility.java
@@ -3,10 +3,7 @@ package net.warcar.uo_uo_no_mi.abilities;
 import net.minecraft.entity.LivingEntity;
 import net.warcar.uo_uo_no_mi.init.UoMorphs;
 import net.warcar.uo_uo_no_mi.mixins.AbilityProjectileMixin;
-import xyz.pixelatedw.mineminenomi.api.abilities.AbilityCategory;
-import xyz.pixelatedw.mineminenomi.api.abilities.AbilityCore;
-import xyz.pixelatedw.mineminenomi.api.abilities.ExplosionAbility;
-import xyz.pixelatedw.mineminenomi.api.abilities.RepeaterAbility2;
+import xyz.pixelatedw.mineminenomi.api.abilities.*;
 import xyz.pixelatedw.mineminenomi.api.abilities.components.RequireMorphComponent;
 import xyz.pixelatedw.mineminenomi.api.helpers.AbilityHelper;
 import xyz.pixelatedw.mineminenomi.entities.projectiles.AbilityProjectileEntity;
@@ -14,7 +11,9 @@ import xyz.pixelatedw.mineminenomi.entities.projectiles.rokushiki.RankyakuProjec
 import xyz.pixelatedw.mineminenomi.entities.projectiles.swordsman.YakkodoriProjectile;
 
 public class KaifuAbility extends RepeaterAbility2 {
-    public static final AbilityCore<KaifuAbility> INSTANCE = new AbilityCore.Builder<>("Kaifu", AbilityCategory.DEVIL_FRUITS, KaifuAbility::new).build();
+    public static final AbilityCore<KaifuAbility> INSTANCE =
+            new AbilityCore.Builder<>("Kaifu", AbilityCategory.DEVIL_FRUITS, KaifuAbility::new)
+                    .addDescriptionLine(AbilityDescriptionLine.NEW_LINE, RequireMorphComponent.getTooltip()).build();
     private final RequireMorphComponent morphComponent;
     public KaifuAbility(AbilityCore<KaifuAbility> core) {
         super(core);
@@ -23,7 +22,8 @@ public class KaifuAbility extends RepeaterAbility2 {
                 this.projectileComponent.shootWithSpread(entity, 2, 1, 3);
             }
         });
-        this.morphComponent = new RequireMorphComponent(this, UoMorphs.SEIRYU_FLY.get(), UoMorphs.SEIRYU_HEAVY.get());
+        this.morphComponent = new RequireMorphComponent(this, UoMorphs.SEIRYU_FLY.get());
+        this.addCanUseCheck(this::beforeUse);
         this.addComponents(morphComponent);
     }
 
@@ -41,7 +41,7 @@ public class KaifuAbility extends RepeaterAbility2 {
 
     public AbilityProjectileEntity getProjectileFactory(LivingEntity livingEntity) {
         AbilityProjectileEntity entity;
-        if (livingEntity.getRandom().nextBoolean()) {
+        if (livingEntity.getRandom().nextBoolean() && !SeiryuHeavyPointAbility.isFullyDrunk(livingEntity)) {
             entity = new RankyakuProjectile(livingEntity.level, livingEntity);
         } else {
             entity = new YakkodoriProjectile(livingEntity.level, livingEntity);
@@ -49,12 +49,18 @@ public class KaifuAbility extends RepeaterAbility2 {
         entity.setMaxLife(100);
         entity.setDamage(65);
         ((AbilityProjectileMixin) entity).setParent(INSTANCE);
-        entity.onBlockImpactEvent = hit -> {
-            ExplosionAbility explosion = AbilityHelper.newExplosion(livingEntity, livingEntity.level, hit.getX(), hit.getY(), hit.getZ(), 3);
-            explosion.setStaticDamage(5);
-            explosion.doExplosion();
-            entity.remove();
-        };
+        entity.setBlocksAffectedLimit(20);
         return entity;
+    }
+
+    private AbilityUseResult beforeUse(LivingEntity entity, IAbility ability) {
+        if (SeiryuHeavyPointAbility.isFullyDrunk(entity) && AbilityHelper.canUseSwordsmanAbilities(entity)) {
+            this.projectileComponent.shoot(entity);
+            this.cooldownComponent.startCooldown(entity, 450);
+            return AbilityUseResult.fail(null);
+        } else if (SeiryuHeavyPointAbility.isFullyDrunk(entity)) {
+            return AbilityHelper.canUseSwordsmanAbilities(entity, ability);
+        }
+        return AbilityUseResult.success();
     }
 }
